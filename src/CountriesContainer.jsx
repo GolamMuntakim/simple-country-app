7b43579783ad785cd920adf01ded311279518c6e
Golam Muntakim <seams123456789@gmail.com>
2024-09-10 16:06:31 -0700

    useEffect hook

--- src/CountriesContainer.jsx
+++ src/CountriesContainer.jsx
@@ -1,11 +1,21 @@
-import dataFile from '../public/all.json'
+// import dataFile from '../public/all.json'
+import { useEffect, useState } from 'react';
 import CountryCard from './CountryCard';
 const CountriesContainer = ({query}) => {
+    const [countriesData, setCountriesData] = useState([])
+ useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then((res)=>res.json())
+    .then((data)=>{
+     setCountriesData(data)
+     console.log(countriesData)
+    })
+ },[])
     return (
         <div> 
             <div className="countries-container">
              {
-                dataFile.filter((country)=>(country?.name?.common.toLowerCase().includes(query))).map((country,idx)=>{
+                countriesData.filter((country)=>(country?.name?.common.toLowerCase().includes(query))).map((country,idx)=>{
                     return <CountryCard key={idx} name={country?.name?.common} flag={country?.flags?.png} 
                     population={country?.population} region={country?.region} capital={country?.capital}></CountryCard>
                })
